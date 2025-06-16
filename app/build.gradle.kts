cb0fea26bed8237cf4b2399f4d839e61b73a5c99
simao <simaomadiadia@gmail.com>
2025-06-16 19:54:19 +0100

    Adicionar Biblioteca Navigation para ou routeamento e navegacao entre as telas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    val nav_version = "2.5.3"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
 }
