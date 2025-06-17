9347cc3964acce3ef3b7a10d475ca1ad4f25b751
simao <simaomadiadia@gmail.com>
2025-06-17 20:04:09 +0100

    adicionar floatingButtonAction ou seja botao adicionar tarefa na tela lista de tarefas

--- app/src/main/java/pt/ipg/listadetarefa/view/ListaTarefas.kt
+++ app/src/main/java/pt/ipg/listadetarefa/view/ListaTarefas.kt
@@ -1,7 +1,9 @@
 package pt.ipg.listadetarefa.view
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,9 +13,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import pt.ipg.listadetarefa.R
 import pt.ipg.listadetarefa.ui.theme.Purple80
 import androidx.navigation.NavController as NavController1
-
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -36,8 +40,19 @@ fun ListaTarefas(navController : NavController1){
                 }
             )
         },
-        containerColor = Color.Black // Cor de fundo do Scaffold
+        containerColor = Color.Black ,// Cor de fundo do Scaffold
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
 
+                },
+                containerColor = Purple80,
+
+            ) {
+                Image(imageVector = ImageVector.vectorResource(id = R.drawable.baseline_add_24), contentDescription = "Adicionar")
+            }
+
+        }
     ){
 
     }
